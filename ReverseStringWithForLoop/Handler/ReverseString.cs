fee2cca7d93d9b96e33c1ec9a3dd03dc7f79c429
Eric.Earley@coxautoinc.com <Eric.Earley@coxautoinc.com>
2022-01-12 10:28:58 -0700

    initial commit

---
+++ ReverseStringWithForLoop/Handler/ReverseString.cs
@@ -0,0 +1,12 @@
+﻿namespace ReverseStringWithForLoop.Handler;
+
+public static class StringExtensionMethods
+{
+    public static string ReverseString(this string? str)
+    {
+        var reversedString = "";
+        if (str == null) return reversedString;
+        for (var i = str.Length - 1; i >= 0; i--) reversedString += str[i];
+        return reversedString;
+    }
+}
---
+++ ReverseStringWithForLoop/Program.cs
@@ -0,0 +1,6 @@
+﻿using ReverseStringWithForLoop.Handler;
+
+Console.Write("Enter any string: ");
+var userInput = Console.ReadLine();
+var reversedString = userInput.ReverseString();
+Console.WriteLine($"{reversedString}");
